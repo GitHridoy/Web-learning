8239fc3f740a812392915ccf66c7c64d0e507d8a
GitHridoy <mdhridoy10000000@gmail.com>
2023-09-08 19:46:14 +0600

    contact

--- js/app.js
+++ js/app.js
@@ -16,3 +16,25 @@ sr.reveal('.contact-box',{delay:350, origin:'top'})
 
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+
+
+
+
+// contact section pop 
+let Pop = document.getElementById('pop');
+let Close = document.getElementById('close');
+var Elements = document.getElementsByTagName('section');
+
+document.getElementById('contact').addEventListener('click', function(){
+Pop.classList.add('show');
+document.body.style.backgroundColor = 'rgb(0, 0, 0, 0.6)';
+Elements.style.display = 'none'
+
+
+})
+
+document.getElementById('close').addEventListener('click', function(){
+Pop.classList.remove('show');
+document.body.style.backgroundColor = 'white'
+
+})
